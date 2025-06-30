0cc85ed9a11b2a4bc5171f9a6378cdaa9eb7eb46
Diego Quispe <diego.quispe.ap@utec.edu.pe>
2025-06-30 02:12:14 -0500

    🗑️ Cleanup+estimate redius

--- front/onlyTest.py
+++ front/onlyTest.py
@@ -1,7 +1,10 @@
 import sys
 import os
+import pandas as pd
+import numpy as np
+from sklearn.neighbors import NearestNeighbors
+from random import sample
 
-# Get the absolute path to the project's root directory
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 build_path = os.path.join(root_dir, "build")
 sys.path.append(build_path)
@@ -10,14 +13,28 @@ try:
     from py_arkade import ArkadeModel, FastRNN
 
     data_path = "../datasets/sample_gowalla.txt"
-    distance_metric = "3"
-    search_radius = 10.0
+    distance_metric = "5"
     num_neighbors = 5
     total_data_points = 1000
     query_points = 10
     with_true_knn = True
     # i had to create it manually
     output_file = "./knn_results9.txt"
+
+    print("Estimating search radius...")
+    df = pd.read_csv(data_path, sep='\t', header=None, nrows=total_data_points)
+    list1 = df.values.tolist()
+
+    samples = sample(list1,100)
+
+    X = np.array(samples)
+
+    nbrs = NearestNeighbors(n_neighbors=2, algorithm='ball_tree', metric='l1').fit(X)
+
+    distances, indices = nbrs.kneighbors(X)
+
+    search_radius = min(distances, key=lambda x: x[1])[1]
+    print(f"Estimated search radius = {search_radius}")
 
     print("Creating ArkadeModel instance...")
 
@@ -38,4 +55,3 @@ try:
 except ImportError as e:
     print(f"Error importing module: {e}")
     sys.exit(1)
-
